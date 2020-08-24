4d3dab08ae7d222ff59174c80e47279130802c57
CJ Dodge <hitmancjack@yahoo.com>
2020-08-24 13:07:00 -0700

    styling the app

--- script.js
+++ script.js
@@ -1,21 +1,23 @@
 var el_down = document.getElementById("guests"); 
   
-//Function for random Password
+
 function generateP() { 
+    //strings of characters from which the password can be generated from
     var pass = ''; 
     var str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' +  
-            'abcdefghijklmnopqrstuvwxyz0123456789@#$'; 
-      
-    for (i = 1; i <= 8; i++) { 
+            'abcdefghijklmnopqrstuvwxyz0123456789@#$!&'; 
+    //Creates function for generating password
+    for (i = 1; i <= 12; i++) { 
         var char = Math.floor(Math.random() 
                     * str.length + 1); 
-          
+    
         pass += str.charAt(char) 
     } 
       
     return pass; 
 } 
 
-function gfg_Run() { 
-    el_down.innerHTML = generateP(); 
+function retrievePassword() { 
+    el_down.innerHTML = generateP();
+     
 } 
